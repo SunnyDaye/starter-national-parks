dad2bcf626d06333bcad1ad099c1f362b48f07a6
Suncerie Daye <sunceried@gmail.com>
2021-03-10 11:28:47 -0600

    daily update

--- index.js
+++ index.js
@@ -42,10 +42,50 @@ for(let i=0;i<areas.length;i++){
 
 //   console.log(desc.innerHTML);
 // }
-let ratings = document.querySelectorAll(".rating .value").values();
-for (let rating of ratings) {
-  let ratingValue = parseFloat(rating.innerText);
-  if (ratingValue > 4.7) {
-    rating.classList.add("high-rating");
-  }
-}
+// let ratings = document.querySelectorAll(".rating .value").values();
+// for (let rating of ratings) {
+//   let ratingValue = parseFloat(rating.innerText);
+//   if (ratingValue > 4.7) {
+//     rating.classList.add("high-rating");
+//   }
+// }
+
+/*************Adding an element to the DOM****************/
+//  const parks =  document.querySelectorAll(".park");
+//  const numOfParks =  parks.length; //Node list can be treated like arrays
+
+//  let newElement = document.createElement("div"); //Think of divs as variables. They are used to hold single data values or a group of values like an object with
+
+//  //Since createElement returns an HTMLElement object, we can add the data we want to present in the document in the innerHTML or innerText property
+//  newElement.innerText = `${numOfParks} fantastic parks to explore!`; 
+
+//  //If you want the element to be styled, create an css class and add it to the element class
+//  newElement.classList.add("header-statement");
+
+//  //The next and final step is to add the element where you want to on the page
+//  // 1: grab the destination element from the DOM. This is where you will insert your new element into
+//  let destinationElement = document.querySelector("header");
+//  //2: add your new element as a child 
+//  destinationElement.appendChild(newElement);
+
+
+/*************Removing an element from the DOM*************/
+//Remove the first park
+
+//Get the parent element
+// const parent = document.querySelector("main");
+
+// //get the child element
+// const child = document.querySelector(".park");
+
+// //remove the child element
+
+// parent.removeChild(child);
+
+
+ 
+
+
+ 
+
+
